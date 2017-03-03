0c0e76a7db38e8a3406fd90097b74d660730367d
Michael Correll <michael.a.correll@gmail.com>
2017-03-03 13:58:21 -0800

    Switching to d3

--- heatmap/heatmap.js
+++ heatmap/heatmap.js
@@ -1,34 +1,60 @@
-function preload(){
+var map = colorbrewer.YlGnBu[9];
 
+var width = 300;
+var height = 300;
+
+var x = d3.scale.linear().range([0,width]);
+var y = d3.scale.linear().range([0,height]);
+var z = d3.scale.quantize().range(map);
+
+var data = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
+
+var uncertainty = [
+  [1,1,1],
+  [1,1,1],
+  [1,1,1]
+];
+
+x.domain([0,data[0].length]);
+y.domain([0,data.length]);
+z.domain([d3.min(d3.min(data)),d3.max(d3.max(data))]);
+
+var w = width/data[0].length;
+var h = height/data.length;
+
+var svg = d3.select("body").append("svg")
+  .attr("width",width)
+  .attr("height",height);
+
+var cells = svg.selectAll("g")
+    .data(data)
+    .enter()
+    .append("g")
+    .selectAll("rect")
+      .data(function(d,i){ return d.map(function(val){ return {r:i, v:val};});})
+      .enter()
+      .append("rect")
+      .datum(function(d,i){ d.c = i; d.u = uncertainty[d.r][d.c]; return d; })
+      .attr("x",function(d){ return x(d.c);})
+      .attr("y",function(d){ return y(d.r);})
+      .attr("width",w)
+      .attr("height",h)
+      .attr("fill",function(d){ return z(d.v);});
+
+
+function lerp(v1,v2,amount){
+       amount = Math.min(Math.max(amount,0),1);
+       return v1 + (v2 - v1) * amount;
 }
 
-function setup(){
-  pixelDensity(displayDensity());
-  createCanvas(500,500);
-  background(255);
-  drawHeatmap([0,0.25,0.5,0.75,1.0,0.1,0.9,0.3]);
-  noLoop();
-}
+function labLerp(c1,c2,amount){
+  //Linearly interpolates two colors in CIELAB space
+  var lc1 = d3.lab(c1);
+  var lc2 = d3.lab(c2);
+  return d3.lab( lerp(lc1.l,lc2.l,amount), lerp(lc1.a,lc2.a,amount), lerp(lc1.b,lc2.b,amount));
 
-function drawHeatmap(samples){
-  var delta, index, perRow, curY;
-  perRow = ceil(sqrt(samples.length));
-  delta = min(width,height)/perRow;
-
-  curY = 0;
-  for(var i = 0;i<perRow;i++){
-    curX = 0;
-    for(var j=0;j<perRow;j++){
-      drawCell(samples[i*perRow + j],0,curX,curY,delta,0);
-      curX+=delta;
-    }
-    curY+=delta;
-  }
-}
-
-function drawCell(value,uncertainty,x,y,w,mode){
-  var fillC = lerpColor(color(255),color(255,0,0),value);
-  noStroke();
-  fill(fillC);
-  rect(x,y,w,w);
 }
