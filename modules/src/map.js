81226b64fe445288a6b7ae03d3bb78f6af2f528a
Michael Correll <michael.a.correll@gmail.com>
2017-08-25 15:34:47 -0700

    untested mapping code

---
+++ modules/src/map.js
@@ -0,0 +1,63 @@
+import * as d3 from "d3";
+
+// Returns a color, based on a bivariate data point,
+// a quantization function, and a color interpolator
+export function simpleMap(m_mode,m_range,m_scale) {
+  var range = m_range,
+      scale = m_scale ? m_scale : function(v,u) {
+        var data = u!=undefined ? {v:v, u:u} : {v:v.v, u:v.u};
+        return data;
+      },
+      mode = m_mode;
+
+  function CIEdist(color1,color2) {
+      var c1 = d3.lab(d3.color(color1)),
+          c2 = d3.lab(d3.color(color2));
+      return Math.sqrt(Math.pow( (c1.l-c2.l),2) + Math.pow(c1.a-c2.a,2) + Math.pow(c1.b - c2.b,2));
+  }
+
+  function map(value,uncertainty) {
+    var data = scale(value,uncertainty);
+    var vcolor = range(data.v);
+    var uScale = d3.scaleLinear().domain([0,1]).range([0.0,1.0]);
+    switch(mode) {
+      case "usl":
+      default:
+        vcolor = d3.interpolateLab(vcolor,"white",uScale(data.u));
+      break;
+    }
+    return vcolor;
+  }
+
+  map.colorList = function() {
+    return scale.range().map(map);
+  }
+
+
+
+  map.colorDists = function() {
+      var clist = this.colorList(),
+          matrix = new Array(clist.length),
+          minDist,
+          minPair = new Array(2),
+          dist;
+
+      for(var i = 0;i<matrix.length;i++){
+        matrix[i] = new Array(clist.length);
+        for(var j = 0;j<matrix[i].length;j++){
+          dist = CIEDist(clist[i],clist[j]);
+          matrix[i][j] = dist;
+          if(i!=j && ((i==0 && j==1) || (dist<minDist))){
+            minDist = dist;
+            minPair = [clist[i],clist[j]];
+          }
+        }
+      }
+
+      matrix.minDist = minDist;
+      matrix.minPair = minPair;
+      return matrix;
+  };
+
+  return map;
+}
