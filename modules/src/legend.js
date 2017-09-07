e89d3ac4638bbf16771804caf03149841033649b
Dominik Moritz <domoritz@gmail.com>
2017-09-06 22:39:10 -0700

    Legend for heatmap

--- modules/src/legend.js
+++ modules/src/legend.js
@@ -3,6 +3,10 @@
 */
 import * as d3 from "d3";
 
+import {simpleHeatmap, simpleArcmap} from './heatmap';
+
+var epsylon = 0.0001;
+
 export function simpleLegend(m_scale,m_size,m_height,m_format,m_title,m_x,m_y) {
   var el = null,
       title = m_title,
@@ -143,62 +147,155 @@ export function simpleLegend(m_scale,m_size,m_height,m_format,m_title,m_x,m_y) {
   return legend;
 };
 
-export function heatmapLegend(m_heatmap) {
-  var heatmap = m_heatmap;
+export function heatmapLegend(data,m_scale,m_size,m_format,m_utitle,m_vtitle,m_x,m_y) {
+  var el = null,
+    utitle = m_utitle ? m_utitle : "Uncertainty",
+    vtitle = m_vtitle ? m_vtitle : "Value",
+    scale = m_scale ? m_scale : null,
+    size = m_size ? m_size : 200,
+    fmat = m_format ? m_format : ".2f",
+    x = m_x ? m_x : 0,
+    y = m_y ? m_y : 0;
 
-  var legend = {};
+  var heatmap = simpleHeatmap(data,m_scale,m_size,null,0,0);
 
-  legend.make = function() {
-    if (!heatmap) {
+  var legend = function(nel) {
+    el = nel;
+    legend.setProperties();
+
+    el.call(heatmap);
+  }
+
+  legend.setProperties = function() {
+    if (!el) {
       return;
     }
-    // TODO: finish
 
-    var svgGroup = heatmap.svg().append("g")
-    var size = heatmap.size;
+    heatmap.data(data);
+    heatmap.scale(scale);
+    heatmap.size(size);
 
-    svgGroup.attr("transform", "translate(-20, -20)");
+    el
+      .attr("class", "legend")
+      .attr("transform", "translate("+x+","+y+")");
 
-    var xAxis = d3.scalePoint().range([0, size]).domain([0,1]);
+    var uStep = 1 / data.length;
+    var uDom = d3.range(0, 1 + uStep - epsylon, uStep);
+  
+    var vStep = 1 / data.length;
+    var vDom = d3.range(0, 1 + vStep - epsylon, vStep);
 
-    svgGroup.append("g")
-      .call(d3.axisTop(xAxis).tickFormat(d3.format("d")));
+    var xAxis = d3.scalePoint().range([0, size]).domain(vDom);
 
-    svgGroup.append("text")
+    el.append("g")
+      .call(d3.axisTop(xAxis).tickFormat(d3.format(fmat)));
+
+    el.append("text")
       .style("text-anchor", "middle")
       .style("font-size", 13)
-      .attr("transform", "translate(" + (size / 2) + ", " + (-30) + ")")
-      .text("Value");
+      .attr("transform", "translate(" + (size / 2) + ", " + (-25) + ")")
+      .text(vtitle);
 
-    var yAxis = d3.scalePoint().range([0, size]).domain([0,1]);
+    var yAxis = d3.scalePoint().range([0, size]).domain(uDom);
 
-    svgGroup.append("g")
+    el.append("g")
       .attr("transform", "translate(" + size + ", 0)")
-      .call(d3.axisRight(yAxis).tickFormat(d3.format("d")));
+      .call(d3.axisRight(yAxis).tickFormat(d3.format(fmat)));
 
-    svgGroup.append("text")
+    el.append("text")
       .style("text-anchor", "middle")
       .style("font-size", 13)
       .attr("transform", "translate(" + (size + 40) + ", " + (size / 2) + ")rotate(90)")
-      .text("Uncertainty");
+      .text(utitle);
   };
 
-  legend.unmake = function() {
-    // TODO
-  };
-
-  legend.heatmap = function(nheatmap) {
+  legend.data = function(newData) {
     if(!arguments.length) {
-      return heatmap;
+      return data;
     }
     else {
-      heatmap = nheatmap;
-      legend.uname();
-      legend.make();
+      data = newData;
+      legend.setProperties();
       return legend;
     }
   };
 
-  legend.make();
+  legend.scale = function(s) {
+    if(!arguments.length) {
+      return scale;
+    }
+    else {
+      scale = s;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.size = function(s) {
+    if(!arguments.length) {
+      return size;
+    }
+    else {
+      size = s;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.format = function(f) {
+    if(!arguments.length) {
+      return fmat;
+    }
+    else {
+      fmat = f;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.x = function(nx) {
+    if(!arguments.length) {
+      return x;
+    }
+    else {
+      x = nx;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.y = function(ny) {
+    if(!arguments.length) {
+      return y;
+    }
+    else {
+      y = ny;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.utitle = function(t) {
+    if(!arguments.length) {
+      return utitle;
+    }
+    else {
+      utitle = t;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
+  legend.vtitle = function(t) {
+    if(!arguments.length) {
+      return vtitle;
+    }
+    else {
+      vtitle = t;
+      legend.setProperties();
+      return legend;
+    }
+  };
+
   return legend;
 };
